0c0f3f0e9f673d172557ec22a093a107e4812f52
Sandesh Shakya <sandeshakya94@gmail.com>
2019-06-28 10:59:01 -0600

    fixing prod script bugs

--- scripts/aws-cli-assumerole.sh
+++ scripts/aws-cli-assumerole.sh
@@ -1,10 +1,11 @@
-unset  AWS_SESSION_TOKEN
+unset AWS_SESSION_TOKEN
 
 temp_role=$(aws sts assume-role \
-                    --role-arn "arn:aws:iam::076923833809:role/circleci_role" \
-                    --role-session-name "circleci-prod" \
-                    --profile sandesh-shakya-prod)
+		--role-arn "arn:aws:iam::076923833809:role/circleci_role" \
+		--role-session-name "circleci-prod" \
+		--profile sandesh-shakya-prod)
 
 export AWS_ACCESS_KEY_ID=$(echo $temp_role | jq .Credentials.AccessKeyId | xargs)
 export AWS_SECRET_ACCESS_KEY=$(echo $temp_role | jq .Credentials.SecretAccessKey | xargs)
 export AWS_SESSION_TOKEN=$(echo $temp_role | jq .Credentials.SessionToken | xargs)
+
